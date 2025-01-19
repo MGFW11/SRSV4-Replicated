3cff02ce363038c52f3b755ef70b5f9bac50268b
MGFW11 <137591439+MGFW11@users.noreply.github.com>
2025-01-19 19:27:37 +0700

    Update Replicated SRSV4 A2.lua

--- Replicated SRSV4 A2.lua
+++ Replicated SRSV4 A2.lua
@@ -307,8 +307,8 @@ end
 
 local radius = 50
 local height = 100
-local rotationSpeed = 50
-local attractionStrength = 5000
+local rotationSpeed = 55
+local attractionStrength = 5500
 local ringPartsEnabled = false
 
 local function RetainPart(Part)
